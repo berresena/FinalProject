e8d53827c41405a8f89d6fe97f9226d3f4c9525d
Berre Sena KIRAÇ <berresenakirac71@gmail.com>
2025-06-04 00:44:50 +0300

    Add files via upload

---
+++ bitirme_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/berre/bitirme_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/berre/bitirme_ws/devel;/home/berre/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/berre/bitirme_ws/devel;/home/berre/catkin_ws/devel;/opt/ros/noetic' != '' else []
